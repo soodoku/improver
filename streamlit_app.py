122f050b8bbf016db36152e1fd6871df658544da
***** <gsood07@gmail.com>
2023-10-02 16:26:11 -0700

    add api key to the session etc.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,37 @@
 import streamlit as st
 import openai
 
+default_prompt = "Highlight any errors in the code and provide suggestions for improving the code. \
+                  Also implement those suggestions and provide revised code."
+
 def generate_text(prompt, api_key):
     openai.api_key = api_key
+    full_prompt = "Default Prompt: " + default_prompt + "\nUser Input: " + prompt
     response = openai.Completion.create(
-        engine="davinci",  # You can experiment with different engines
-        prompt=prompt,
-        max_tokens=100  # Adjust max tokens as needed
+        engine="davinci",
+        prompt=full_prompt,
+        max_tokens=150
     )
     return response.choices[0].text.strip()
 
+def get_set_api_key(api_key=None):
+    session_state = Server.get_current()._session_state
+    if api_key:
+        session_state.api_key = api_key
+    return session_state.api_key
+
 st.title("Suggestions for Improving Your Code")
 
-default_prompt = "Highlight any errors in the code and provide suggestions for improving the code. Also implement those suggestions and provide revised code."
-user_input = st.text_input("Enter your code", default_prompt)
+user_input = st.text_area("Enter your code:", height=200)
 st.write("You entered:", user_input)
+
+api_key = get_set_api_key()
 
 api_key = st.text_input("Enter your OpenAI API key", type="password")
 st.write("API Key entered:", api_key)
+get_set_api_key(api_key)
 
-if st.button("Generate Text") and api_key:
+if st.button("Generate Suggestions for Improvement") and api_key:
     with st.spinner("Generating..."):
         generated_text = generate_text(user_input, api_key)
     st.success("Text Generated!")
